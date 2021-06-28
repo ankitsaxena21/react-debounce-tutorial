73bf4362802463fead2665c4b67ee81fa5ffc9d8
Ankit Saxena <47791661+ankitsaxena21@users.noreply.github.com>
2021-06-28 14:07:35 +0530

    Update debounceScratch.js

--- src/debounceScratch.js
+++ src/debounceScratch.js
@@ -1,22 +1,22 @@
 import React, { useCallback, useState } from "react";
-import { debounce } from "lodash";
-import { DebounceInput } from 'react-debounce-input';
+// import { debounce } from "lodash";
+// import { DebounceInput } from 'react-debounce-input';
 
 const DebounceSrcatch = () => {
   const [suggestions, setSuggestions] = useState("");
 
   // Method 1 - from scratch
-  // const debounce = (func) => {
-  //   let timer;
-  //   return function (...args) {
-  //     const context = this;
-  //     if (timer) clearTimeout(timer);
-  //     timer = setTimeout(() => {
-  //       timer = null;
-  //       func.apply(context, args);
-  //     }, 500);
-  //   };
-  // };
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = null;
+        func.apply(context, args);
+      }, 500);
+    };
+  };
 
   const handleChange = (value) => {
     fetch(`https://demo.dataverse.org/api/search?q=${value}`)
@@ -24,23 +24,28 @@ const DebounceSrcatch = () => {
       .then((json) => setSuggestions(json.data.items));
   };
 
-  // const optimizedFn = useCallback(debounce(handleChange), []);
-
+  const optimizedFn = useCallback(debounce(handleChange), []);
 
   // Method 2 - using lodash
-  const handleChangeWithLib = debounce((value) => {
-    fetch(`https://demo.dataverse.org/api/search?q=${value}`)
-      .then((res) => res.json())
-      .then((json) => setSuggestions(json.data.items));
-  }, 500);
-
-
-
+  // const handleChangeWithLib = debounce((value) => {
+  //   fetch(`https://demo.dataverse.org/api/search?q=${value}`)
+  //     .then((res) => res.json())
+  //     .then((json) => setSuggestions(json.data.items));
+  // }, 500);
 
   return (
     <>
       <h2 style={{ textAlign: "center" }}>Debouncing in React JS</h2>
 
+      {/* Method 1 - from scratch */}
+      <input
+        type="text"
+        className="search"
+        placeholder="Enter something here..."
+        onChange={(e) => optimizedFn(e.target.value)}
+      />
+
+      {/* Method 2 - using lodash */}
       {/* <input
         type="text"
         className="search"
@@ -49,12 +54,12 @@ const DebounceSrcatch = () => {
       /> */}
 
       {/* Method 3 - using react-debounce-input */}
-      <DebounceInput
+      {/* <DebounceInput
         minLength={2}
         className="search"
         placeholder="Enter something here..."
         debounceTimeout={500}
-        onChange={e => handleChange(e.target.value)} />
+        onChange={e => handleChange(e.target.value)} /> */}
 
       {suggestions.length > 0 && (
         <div className="autocomplete">
